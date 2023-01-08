b4ea4aa0c147ff7aa8577ef18001da0de06c389e
Maxim Vorobei <maxvorobei15@gmail.com>
2023-01-09 02:20:13 +0300

    feat points task

---
+++ Points.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Points
+{
+    internal class Point
+    {
+        public int x;
+        public int y;
+
+        public string GetInfo()
+        {
+            return "Point coordinates: (" + x + ", " + y + ")";
+        }
+
+        public void RandomCoordinates()
+        {
+            Random mark = new Random();
+     
+            this.x = mark.Next(-25, 26);
+            this.y = mark.Next(-25, 26);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,50 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace typeMinMaxValue
+namespace Points
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            byte maxByteValue = byte.MaxValue;
-            byte minByteValue = byte.MinValue;
-            Console.WriteLine("max Byte value: " + maxByteValue + ", min Byte value: " + minByteValue);
 
-            short maxShortValue = short.MaxValue;
-            short minShortValue = short.MinValue;
-            Console.WriteLine("max Short value: " + maxShortValue + ", min Short value: " + minShortValue);
+            Point dot1 = new Point();
+            dot1.RandomCoordinates();
+            Console.WriteLine(dot1.GetInfo());
 
-            ushort maxUshortValue = ushort.MaxValue;
-            ushort minUshortValue = ushort.MinValue;
-            Console.WriteLine("max Ushort value: " + maxUshortValue + ", min Ushort value: " + minUshortValue);
 
-            int maxIntValue = int.MaxValue;
-            int minIntValue = int.MinValue;
-            Console.WriteLine("max Int value: " + maxIntValue + ", min Int value: " + minIntValue);
+            Point dot2 = new Point();
+            dot2.RandomCoordinates();
+            Console.WriteLine(dot2.GetInfo());
 
-            uint maxUintValue = uint.MaxValue;
-            uint minUintValue = uint.MinValue;
-            Console.WriteLine("max Uint value: " + maxUintValue + ", min Uint value: " + minUintValue);
-
-            long maxLongValue = long.MaxValue;
-            long minLongValue = long.MinValue;
-            Console.WriteLine("max Long value: " + maxLongValue + ", min Long value: " + minLongValue);
-
-            ulong maxUlongValue = ulong.MaxValue;
-            ulong minUlongValue = ulong.MinValue;
-            Console.WriteLine("max Ulong value: " + maxUlongValue + ", min Ulong value: " + minUlongValue);
-
-            float maxFloatValue = float.MaxValue;
-            float minFloatValue = float.MinValue;
-            Console.WriteLine("max Float value: " + maxFloatValue + ", min Float value: " + minFloatValue);
-
-            double maxDoubleValue = double.MaxValue;
-            double minDoubleValue = double.MinValue;
-            Console.WriteLine("max Double value: " + maxDoubleValue + ", min Double value: " + minDoubleValue);
-
-            char maxCharValue = char.MaxValue;
-            char minCharValue = char.MinValue;
-            Console.WriteLine("max Char value: " + maxCharValue + ", min Char value: " + minCharValue);
+            static double BetweenLength(Point dot1, Point dot2)
+            {
+                return Math.Sqrt(Math.Pow(dot2.x - dot1.x, 2) + Math.Pow(dot2.y - dot1.y, 2));
+            }
+            Console.WriteLine(BetweenLength(dot1, dot2));
         }
     }
 }
